0788a2e223ae8053fd6d539e28f520faa7bb9a86
Freddy Canido <freddy.canido12@gmail.com>
2021-09-09 15:42:14 -0400

    changed spacing

--- src/com/company/cardealer/interfaces/languages/Language.java
+++ src/com/company/cardealer/interfaces/languages/Language.java
@@ -4,48 +4,27 @@ import java.util.*;
 
 public interface Language {
     String VEHICLETYPE_PROMPT();
-
     String ENTER_PROMPT();
-
     List<String> PLAYER_FIELDS();
-
     List<String> CAR_DEALER_MENU();
-
     String SELECT_PROMPT();
-
     List<String> ERROR_MSGS();
-
     String BUY_VEHICLE_PROMPT();
-
     String SELL_VEHICLE_PROMPT();
-
     String BUY_ENGINE_PROMPT();
-
     String VEHICLE_SOLD();
-
     String ENGINE_SOLD();
-
     List<String> CONTROL_MENU();
-
     List<String> END();
-
     String HAS();
-
     String PASSENGERS();
-
     String HUD();
-
     List<String> TIME();
-
     String SECONDS();
-
     String JUMPOUT_AIR();
-
     String JUMPOUT_LAND();
-
     String YOU_HAVE();
-
     String HEALTH();
-
     List<String> DISPLAY_RACE_RESULT();
+
 }
